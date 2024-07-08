3407030dd3a86e2b14fd225221da9cb3dfa1cf08
Danilo Collares <daniloccastro@dcc.ufrj.br>
2024-07-08 19:24:57 -0300

    Created dialog deletion component and added to merch and notices page

--- src/views/MerchandiseTeamView.vue
+++ src/views/MerchandiseTeamView.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <v-snackbar v-model="snackbar" :timeout="2000">{{ message }}</v-snackbar>
         <v-btn color="#FF7A00" size="large" variant="flat">
             Adicionar mercadoria
         </v-btn>
@@ -8,30 +9,49 @@
             <v-card-subtitle class="mb-4">{{ formatPrice(merch.price)}}</v-card-subtitle>
             <v-card-actions class="d-flex justify-space-evenly">
                 <v-btn text="Editar" variant="flat"></v-btn>
-                <v-btn text="Excluir" variant="text" @click='deleteMerch(merch)'></v-btn>
+                <v-btn text="Excluir" variant="text" @click='openDialog(merch)'></v-btn>
             </v-card-actions>
         </v-card>
+        <DeleteItemDialog 
+        :showDialog='dialogDelete'
+        @cancel-deletion='dialogDelete = false'
+        @delete-item='deleteMerch(selectedMerch)'
+        />
     </div>
 </template>
 <script>
+import DeleteItemDialog from '../components/DeleteItemDialog.vue'
 import EventService from '../services/event.service.js';
 export default {
+    components: {
+        DeleteItemDialog,
+    },
     data() {
         return {
-            merches: []
+            merches: [],
+            selectedMerch: null,
+            dialogDelete: false,
+            snackbar: false,
+            message: '',
         }
     },
     methods: {
         deleteMerch(merch) {
             EventService.deleteMerch(this.$route.params.slug, merch.id).then((response) => {
-                if (response.message) {
-                    this.merches.splice(this.merches.indexOf(merch),1);
-                }
+                this.merches.splice(this.merches.indexOf(merch),1);
+                this.message = response.message;
+                this.snackbar = true;
+                this.dialogDelete = false;
             })
         },
 
         formatPrice(cents) {
             return `R$ ${(cents / 100).toFixed(2)}`;
+        },
+
+        openDialog(merch) {
+            this.selectedMerch = merch;
+            this.dialogDelete = true;
         }
     },
     mounted() {
