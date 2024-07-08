f9a49dfdb6f8e6460bccfa2bfaf19d969669984a
Danilo Collares <daniloccastro@dcc.ufrj.br>
2024-07-07 22:38:43 -0300

    merch listing and deletion

--- src/services/event.service.js
+++ src/services/event.service.js
@@ -34,6 +34,12 @@ class EventService{
         .then(response => response.data)
     }
 
+    deleteMerch(event_slug, merch_id) {
+        return axios
+        .delete(API_URL + 'events/' + event_slug + '/merches/' + merch_id, { headers: authHeader() })
+        .then(response => response.data)
+    }
+
     getEventNotices(event_slug){
         return axios
         .get(API_URL + 'events/' + event_slug + '/notifications', { headers: authHeader() })
