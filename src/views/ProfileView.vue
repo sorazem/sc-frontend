804f3fdfb722efd2acaf579f3b9a626d05de5a2d
Stephanie <stephanieorazem@jovensgenios.com>
2024-07-24 19:46:41 -0300

    :sparkles: edit profile data

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,23 +1,73 @@
 <template>
     <div class="pa-4 content text-left">
-        <!-- <v-img class="hidden-sm-and-down" src="../../src/assets/user-contact-icon.gif" :width="300" /> -->
-        <v-img class="hidden-sm-and-down" src="../../src/assets/profile.png" :width="300" />
-        <h3>Nome</h3>
-        <v-text-field v-model="user.name" :readonly="!editMode" variant="outlined" density="compact"></v-text-field>
-        <h3>Email</h3>
-        <v-text-field v-model="user.email" :readonly="!editMode" variant="outlined" density="compact"></v-text-field>
-        <h3>Ocupação</h3>
-        <v-text-field v-if="!editMode" :readonly="!editMode" variant="outlined" density="compact">{{ user.ocupation }}</v-text-field>
-        <v-select v-if="editMode" v-model="user.ocupation" :items="ocupacaoItems" variant="outlined" density="compact" @change="resetInstitution()"></v-select>
-        <h3>Instituição</h3>
-        <v-text-field v-if="!editMode" :readonly="!editMode" variant="outlined" density="compact">{{ user.institution }}</v-text-field>
-        <v-select v-if="editMode" v-model="user.institution" :items="user.ocupation == 'Estudante de ensino médio'? instituicaoMedioItems : instituicaoSuperiorItems" variant="outlined" density="compact"></v-select>
-        <h3 v-if="user?.dre">DRE</h3>
-        <v-text-field v-if="user?.dre" v-model="user.dre" :readonly="!editMode" variant="outlined" density="compact"></v-text-field>
-        <v-divider class="my-8"></v-divider>
-        <v-btn v-if="!editMode" variant='flat' color="#9C66BD" @click='editMode=true'>Editar dados</v-btn>
-        <v-btn v-if="editMode" variant='outlined' color="#9C66BD" @click='editAccount()'>Salvar alterações</v-btn>
-        <v-btn class="d-block mt-8" variant='flat' color="#9C66BD" @click='showDialog=true'>Deletar conta</v-btn>
+        <v-form v-model="form">
+            <v-img class="hidden-sm-and-down" src="../../src/assets/profile.png" :width="300" />
+            <h3>Nome</h3>
+            <v-text-field 
+                v-model="user.name" 
+                :readonly="!editMode" 
+                variant="outlined" 
+                density="compact"
+                :rules="[required]"
+            ></v-text-field>
+            <h3>Email</h3>
+            <v-text-field 
+                v-model="user.email" 
+                :readonly="!editMode" 
+                variant="outlined" 
+                density="compact"
+                :rules="[required]"
+            ></v-text-field>
+            <h3>Ocupação</h3>
+            <v-text-field 
+                v-if="!editMode" 
+                :readonly="!editMode" 
+                variant="outlined" 
+                density="compact"
+            >
+                {{ user.ocupation }}
+            </v-text-field>
+            <v-select 
+                v-if="editMode" 
+                v-model="user.ocupation" 
+                :items="ocupacaoItems" 
+                variant="outlined" 
+                density="compact" 
+                @update:model-value="resetInstitution"
+                :rules="[required]"
+            ></v-select>
+            <h3>Instituição</h3>
+            <v-text-field 
+                v-if="!editMode" 
+                :readonly="!editMode" 
+                variant="outlined" 
+                density="compact"
+            >
+                {{ user.institution }}
+            </v-text-field>
+            <v-select 
+                v-if="editMode" 
+                v-model="user.institution" 
+                :items="user.ocupation == 'Estudante de ensino médio'? instituicaoMedioItems : instituicaoSuperiorItems" 
+                variant="outlined" 
+                density="compact" 
+                @update:model-value="resetDRE"
+                :rules="[required]">
+            </v-select>
+            <h3 v-if="user?.dre || user.institution == 'UFRJ'">DRE</h3>
+            <v-text-field 
+                v-if="user?.dre || user.institution == 'UFRJ'" 
+                v-model="user.dre" 
+                :readonly="!editMode" 
+                variant="outlined" 
+                density="compact"
+                :rules="[required]">
+            </v-text-field>
+            <v-divider class="my-8"></v-divider>
+            <v-btn v-if="!editMode" variant='flat' color="#9C66BD" @click='editMode=true'>Editar dados</v-btn>
+            <v-btn v-if="editMode" variant='outlined' color="#9C66BD" @click='editAccount()' :disabled="!form">Salvar alterações</v-btn>
+            <v-btn class="d-block mt-8" variant='flat' color="#9C66BD" @click='showDialog=true'>Deletar conta</v-btn>
+        </v-form>
         <v-dialog :modelValue="showDialog" width="auto">
             <v-card max-width="400" text="Quer mesmo excluir sua conta? Você não pode voltar atrás nesta ação" >
                 <template v-slot:actions>
@@ -33,12 +83,13 @@ import UserService from '@/services/user.service';
 export default {
     data(){
         return{
+            form: false,
             showDialog: false,
             editMode: false,
             user: null,
             ocupacaoItems: ['Estudante de ensino superior', 'Estudante de ensino médio', 'Professor', 'Outro'],
-            instituicaoSuperiorItems: ['UFRJ', 'UFF', 'UERJ', 'UFRRJ', 'PUC', 'UNIRIO', 'UVA', 'Estácio de Sá', 'CEFET', 'FAETEC', 'Pedro II', 'Outros', ''],
-            instituicaoMedioItems: ['Pedro II', 'CEFET', 'CAp UFRJ', 'Outros', '']
+            instituicaoSuperiorItems: ['UFRJ', 'UFF', 'UERJ', 'UFRRJ', 'PUC', 'UNIRIO', 'UVA', 'Estácio de Sá', 'CEFET', 'FAETEC', 'Pedro II', 'Outros'],
+            instituicaoMedioItems: ['Pedro II', 'CEFET', 'CAp UFRJ', 'Outros']
         }
     },
     computed:{
@@ -47,9 +98,15 @@ export default {
         }
     },
     methods:{
+        required (v) {
+            return !!v || 'Este campo é obrigatório'
+        },
         resetInstitution(){
-            console.log("reset")
             this.user.institution = '';
+            this.user.dre = null;
+        },
+        resetDRE(){
+            this.user.dre = null;
         },
         deleteAccount(){
             UserService.deleteAccount(this.currentUser.id).then(
